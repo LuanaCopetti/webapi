8b4765d174be86ce66b7fb6eb9f3b8b390e40abf
Luana Copetti <86331342+LuanaCopetti@users.noreply.github.com>
2022-10-10 15:01:00 -0300

    Add files via upload

---
+++ webapi/APIProdutos/Controllers/ProdutosController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using APIProdutos.Business;
+using APIProdutos.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace APIProdutos.Controllers
+{
+    [Authorize("Bearer")]
+    [Route("api/[controller]")]
+    public class ProdutosController : Controller
+    {
+        private ProdutoService _service;
+
+        public ProdutosController(ProdutoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Produto> Get()
+        {
+            return _service.ListarTodos();
+        }
+
+        [HttpGet("{codigoBarras}")]
+        public IActionResult Get(string codigoBr)
+        {
+            var produto = _service.Obter(codigoBr);
+            if (produto != null)
+                return new ObjectResult(produto);
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public Resultado Post([FromBody]Produto produto)
+        {
+            return _service.Incluir(produto);
+        }
+
+        [HttpPut]
+        public Resultado Put([FromBody]Produto produto)
+        {
+            return _service.Atualizar(produto);
+        }
+
+        [HttpDelete("{codigobr}")]
+        public Resultado Delete(string codigobr)
+        {
+            return _service.Excluir(codigobr);
+        }
+    }
+}
---
+++ webapi/APIProdutos/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿namespace APIProdutos.Models
+{
+    public class Produto
+    {
+        private string _codigoBr;
+        public string CodigoBr
+        {
+            get => _codigoBr;
+            set => _codigoBr = value?.Trim().ToUpper();
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get => _nome;
+            set => _nome = value?.Trim();
+        }
+
+        public double Preco { get; set; }
+    }
+}
